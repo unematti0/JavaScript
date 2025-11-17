8d328feafac9dd06d87b0d9f18df77084e9c3bbe
unmeatti0 <mattias.elmers@gmail.com>
2025-11-17 15:36:36 +0200

    esimene commit (1-2)

---
+++ yl01.js
@@ -0,0 +1,30 @@
+/*
+Mattias Elmers
+17.11.2025
+yl1
+*/
+
+const Knr = 4.2
+console.log(typeof Knr)
+
+const toit = 'Will Code 4 food'
+console.log(typeof toit)
+
+const ukk = "123"
+console.log(typeof ukk)
+
+
+const tr = true
+console.log(typeof tr)
+
+const ukks = [1,2,3]
+console.log(typeof ukks)
+
+const f = false
+console.log(typeof f)
+
+const fk = 'true'
+console.log(typeof fk)
+
+const u = undefined
+console.log(typeof u)
---
+++ yl02.js
@@ -0,0 +1,20 @@
+/*
+Mattias Elmers
+17.11.2025
+yl2
+*/
+
+let tund ="15:"
+let minut = "05:"
+let sekundid = 56
+let aeg = tund + minut + sekundid;
+console.log(aeg)
+
+let tsitaat = "pull out"
+console.log ("Ott Tammik: " + tsitaat)
+
+let enimi = "Mattias"
+let pnimi = "Elmers"
+console.log ("Mattias Elmers nime tähed on " + enimi[0] + "." + pnimi[0] + ".")
+
+let nimi = pnimi + "," + enimi
