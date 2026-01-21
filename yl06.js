20cff179fc303511904722620c4415fc3efb0595
unmeatti0 <mattias.elmers@gmail.com>
2026-01-21 11:30:08 +0200

    yl06_pooleli

---
+++ yl06.js
@@ -0,0 +1,18 @@
+let number = prompt("Sisesta number!")
+
+switch (true) {
+case number>0:
+   console.log("arv on positiivne")
+   break;
+case number<0:
+   console.log("arv on negatiivne")
+   break;
+case number == 0:
+   console.log("arv on 0")
+   break;
+default:
+   console.log("Pead sisestama arvu!")
+}
+
+let inimesed = prompt("Mitu inimest on:")
+
