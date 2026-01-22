b5fcf48dd60b09a4fabe70638011b490b6d93492
Mattias Elmers <mattias.elmers@gmail.com>
2026-01-22 18:05:25 +0200

    yl07_pooleli

--- yl06.js
+++ yl06.js
@@ -14,5 +14,22 @@ default:
    console.log("Pead sisestama arvu!")
 }
 
-let inimesed = prompt("Mitu inimest on:")
+let broneeringu_arv = prompt("Mitu inimest on:")
 
+switch (broneeringu_arv) {
+case "1" :
+case "2" :
+console.log("Valige laud kahele inimesele")
+break;
+case "3" :
+case "4" :
+console.log("Valige laud neljale inimesele")
+break;
+case "5" :
+case "6" :
+console.log("Valige laud kuuele inimesele")
+break;
+
+default:
+    console.log("valige suur laud")
+}
