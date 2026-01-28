879b6c9bcc5ddd9f0aff03f183f9f8bf156c919c
Mattias Elmers <mattias.elmers@gmail.com>
2026-01-28 16:20:26 +0200

    yl9_algus

--- yl08.js
+++ yl08.js
@@ -1,7 +1,17 @@
 const raha = [200, 0.2, 10, 0.01, 2, 1, 0.1, 0.02, 0.05, 100, 5, 0.5, 50, 20]
-const munt = 0
-while (raha = 0.2){
 
+const mundid = []
+let i = 0
+let summa = 0
 
-
+while (i < raha.length){
+    if ( 1> raha[i]){
+         mundid.push(raha[i]);
+         summa += raha[i]
+    }
+    i++
 }
+
+console.log(mundid)
+console.log("müntie summa: " + summa)
+console.log("müntide arv: " + mundid.length)
